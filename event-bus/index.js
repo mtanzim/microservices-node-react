6478e4029309461aff8b3fc0ca8e2e097d9dc3e1
Tanzim Mokammel <mtanzim@gmail.com>
2023-01-15 18:10:07 -0500

    simple query service

--- event-bus/index.js
+++ event-bus/index.js
@@ -8,17 +8,23 @@ app.use(bodyParser.json());
 app.post("/events/", (req, res) => {
   const event = req.body;
   axios
-    .post("http://localhost:4000", event)
-    .then(() => console.log("events sent event"))
-    .catch(console.error);
+    .post("http://localhost:4000/events/", event)
+    .then(() => console.log("events sent event to posts"))
+    .catch(() => {
+      console.log("Failed to send to post")
+    });
   axios
-    .post("http://localhost:4001", event)
-    .then(() => console.log("events sent event"))
-    .catch(console.error);
+    .post("http://localhost:4001/events/", event)
+    .then(() => console.log("events sent event to comments"))
+    .catch(() => {
+      console.log("Failed to send to comments")
+    });
   axios
-    .post("http://localhost:4002", event)
-    .then(() => console.log("events sent event"))
-    .catch(console.error);
+    .post("http://localhost:4002/events/", event)
+    .then(() => console.log("events sent event to query"))
+    .catch(() => {
+      console.log("Failed to send to query")
+    });
   res.json({ status: "OK" });
 });
 
---
+++ query/index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.json());
+let posts = {};
+app
+  .post("/events/", (req, res) => {
+    const body = req.body;
+    if (body?.type === "PostCreated") {
+      posts = {
+        ...posts,
+        [body.data.id]: {
+          ...body.data,
+          comments: [],
+        },
+      };
+    }
+    if (body?.type === "CommentCreated") {
+      const curPost = posts[body.data.postId];
+      posts = {
+        ...posts,
+        [body.data.postId]: {
+          ...curPost,
+          comments: curPost.comments.concat(body.data),
+        },
+      };
+    }
+    console.log(posts);
+    res.send("OK");
+  })
+  .get("/posts", (req, res) => {
+    return res.json(posts);
+  });
+
+app.listen(4002, () => {
+  console.log("Posts listening on :4002");
+});
