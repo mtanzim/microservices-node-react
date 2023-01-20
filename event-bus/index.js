1c1de019c757e61aa7c77067a45ea5586d6c6328
Tanzim Mokammel <mtanzim@gmail.com>
2023-01-20 15:09:24 -0500

    event persistence and query service recovery

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,34 +5,41 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
-app.post("/events/", (req, res) => {
-  const event = req.body;
-  axios
-    .post("http://localhost:4000/events/", event)
-    .then(() => console.log("events sent event to posts"))
-    .catch(() => {
-      console.log("Failed to send to post");
-    });
-  axios
-    .post("http://localhost:4001/events/", event)
-    .then(() => console.log("events sent event to comments"))
-    .catch(() => {
-      console.log("Failed to send to comments");
-    });
-  axios
-    .post("http://localhost:4002/events/", event)
-    .then(() => console.log("events sent event to query"))
-    .catch(() => {
-      console.log("Failed to send to query");
-    });
-  axios
-    .post("http://localhost:4003/events/", event)
-    .then(() => console.log("events sent event to moderation"))
-    .catch(() => {
-      console.log("Failed to send to query");
-    });
-  res.json({ status: "OK" });
-});
+const events = [];
+
+app
+  .post("/events/", (req, res) => {
+    const event = req.body;
+    events.push(event);
+    axios
+      .post("http://localhost:4000/events/", event)
+      .then(() => console.log("events sent event to posts"))
+      .catch(() => {
+        console.log("Failed to send to post");
+      });
+    axios
+      .post("http://localhost:4001/events/", event)
+      .then(() => console.log("events sent event to comments"))
+      .catch(() => {
+        console.log("Failed to send to comments");
+      });
+    axios
+      .post("http://localhost:4002/events/", event)
+      .then(() => console.log("events sent event to query"))
+      .catch(() => {
+        console.log("Failed to send to query");
+      });
+    axios
+      .post("http://localhost:4003/events/", event)
+      .then(() => console.log("events sent event to moderation"))
+      .catch(() => {
+        console.log("Failed to send to query");
+      });
+    res.json({ status: "OK" });
+  })
+  .get("/events", (req, res) => {
+    res.json(events);
+  });
 
 app.listen(4005, () => {
   console.log("events listening on :4005");
--- query/index.js
+++ query/index.js
@@ -1,54 +1,59 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const axios = require("axios");
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 let posts = {};
+
+const handleEvent = (body) => {
+  if (body?.type === "PostCreated") {
+    posts = {
+      ...posts,
+      [body.data.id]: {
+        ...body.data,
+        comments: [],
+      },
+    };
+  }
+  if (body?.type === "CommentCreated") {
+    const curPost = posts[body.data.postId];
+    posts = {
+      ...posts,
+      [body.data.postId]: {
+        ...curPost,
+        comments: curPost.comments.concat(body.data),
+      },
+    };
+  }
+  if (body?.type === "CommentUpdated") {
+    console.log("updating");
+    console.log(body);
+    const curPost = posts[body.data.postId];
+    console.log(curPost);
+
+    const curCommentIdx = curPost.comments.findIndex(
+      (c) => c.id === body.data.id
+    );
+    console.log(curPost?.comments?.[curCommentIdx]);
+    posts = {
+      ...posts,
+      [body.data.postId]: {
+        ...curPost,
+        comments: curPost.comments
+          .slice(0, curCommentIdx)
+          .concat(body.data)
+          .concat(curPost.comments.slice(curCommentIdx + 1)),
+      },
+    };
+  }
+};
+
 app
   .post("/events/", (req, res) => {
-    const body = req.body;
-    if (body?.type === "PostCreated") {
-      posts = {
-        ...posts,
-        [body.data.id]: {
-          ...body.data,
-          comments: [],
-        },
-      };
-    }
-    if (body?.type === "CommentCreated") {
-      const curPost = posts[body.data.postId];
-      posts = {
-        ...posts,
-        [body.data.postId]: {
-          ...curPost,
-          comments: curPost.comments.concat(body.data),
-        },
-      };
-    }
-    if (body?.type === "CommentUpdated") {
-      console.log("updating");
-      console.log(body);
-      const curPost = posts[body.data.postId];
-      console.log(curPost);
-
-      const curCommentIdx = curPost.comments.findIndex(
-        (c) => c.id === body.data.id
-      );
-      console.log(curPost?.comments?.[curCommentIdx]);
-      posts = {
-        ...posts,
-        [body.data.postId]: {
-          ...curPost,
-          comments: curPost.comments
-            .slice(0, curCommentIdx)
-            .concat(body.data)
-            .concat(curPost.comments.slice(curCommentIdx + 1)),
-        },
-      };
-    }
+    handleEvent(req.body);
     console.log(posts);
     res.send("OK");
   })
@@ -56,6 +61,12 @@ app
     return res.json(posts);
   });
 
-app.listen(4002, () => {
+app.listen(4002, async () => {
+  const events = await axios.get("http://localhost:4005/events/");
+  // console.log(events.data);
+  events.data.forEach((event) => {
+    handleEvent(event);
+  });
   console.log("Posts listening on :4002");
+
 });
